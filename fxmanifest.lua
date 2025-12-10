de76d867aa48d23b7b8a0a34203ecbd8cdd45e30
Maximus7474 <the.maximus.7474@gmail.com>
2025-12-10 18:53:23 +0100

    tweak(fxmanifest): added more metadata

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,11 @@ fx_version 'cerulean'
 games { 'gta5', 'rdr2' }
 server_only 'yes'
 
+version '0.0.0'
+author 'Maximus7474'
+description 'Basic script to "dump" your database to create backups within fxserver'
+repository 'https://github.com/Maximus7474/cfx-db-backuper'
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
 	'config.lua',
