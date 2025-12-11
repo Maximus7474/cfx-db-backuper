deae0ddab71ceeddc4a3f766f7403d0393f20e03
Maximus7474 <the.maximus.7474@gmail.com>
2025-12-11 23:46:20 +0100

    feat(config): exclusive and excluded table lists

--- config.lua
+++ config.lua
@@ -1,4 +1,14 @@
 Config = {
 	-- Maximum size of a query when creating a backup
 	QuerySize = 100,
+
+	-- Array of table names to not backup
+	-- ExcludedTables = {
+	-- 	'example_tablename',
+	-- },
+
+	-- Array of tables to backup, no other tables will be backed up !
+	-- ExclusiveTables = {
+	-- 	'users',
+	-- },
 }
